5f4e72042b2a8536f5424ed846919ba571882466
ravi2krishna <ravi2krishna@gmail.com>
2025-04-15 08:33:49 +0530

    create variables

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+# Variables
+
+# AWS Access Key ID
+variable aws_access_key {
+  description = "Please Input AWS Access Key ID"
+}
+
+# AWS Secret Key ID
+variable aws_secret_key {
+  description = "Please Input AWS Secret Key ID"
+}
