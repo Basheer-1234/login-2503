853676b288e22b2bfa98487bcc2ab6e3315e3a72
ravi2krishna <ravi2krishna@gmail.com>
2025-04-17 08:48:37 +0530

    create web nsg - http rule

--- terraform/login-infra.tf
+++ terraform/login-infra.tf
@@ -81,3 +81,18 @@ resource "azurerm_network_security_rule" "login-web-nsg-ssh" {
   resource_group_name         = azurerm_resource_group.login-rg.name
   network_security_group_name = azurerm_network_security_group.login-web-nsg.name
 }
+
+# Web NSG - HTTP rule
+resource "azurerm_network_security_rule" "login-web-nsg-http" {
+  name                        = "login-web-http"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "80"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.login-rg.name
+  network_security_group_name = azurerm_network_security_group.login-web-nsg.name
+}
