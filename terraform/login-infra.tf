7530f03bca8684b6dd0680941c832eae3e688e59
ravi2krishna <ravi2krishna@gmail.com>
2025-04-17 08:53:50 +0530

    create db nsg - ssh rule

--- terraform/login-infra.tf
+++ terraform/login-infra.tf
@@ -140,3 +140,18 @@ resource "azurerm_network_security_group" "login-db-nsg" {
   location            = azurerm_resource_group.login-rg.location
   resource_group_name = azurerm_resource_group.login-rg.name
 }
+
+# DB NSG - SSH rule
+resource "azurerm_network_security_rule" "login-db-nsg-ssh" {
+  name                        = "login-db-ssh"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.login-rg.name
+  network_security_group_name = azurerm_network_security_group.login-web-nsg.name
+}
