30151c33eea320d56b2c132ad8ca7dd6af390b00
ravi2krishna <ravi2krishna@gmail.com>
2025-04-17 08:53:27 +0530

    create db nsg

--- terraform/login-infra.tf
+++ terraform/login-infra.tf
@@ -133,3 +133,10 @@ resource "azurerm_network_security_rule" "login-api-nsg-http" {
   resource_group_name         = azurerm_resource_group.login-rg.name
   network_security_group_name = azurerm_network_security_group.login-web-nsg.name
 }
+
+# DB NSG
+resource "azurerm_network_security_group" "login-db-nsg" {
+  name                = "login-db-nsg"
+  location            = azurerm_resource_group.login-rg.location
+  resource_group_name = azurerm_resource_group.login-rg.name
+}
