7b81e59a11c14face0730118ff81ad62581c0ec7
ravi2krishna <ravi2krishna@gmail.com>
2025-04-22 08:44:40 +0530

    update vpc_tenancy

--- terraform/infra.tf
+++ terraform/infra.tf
@@ -1,7 +1,7 @@
 # VPC
 resource "aws_vpc" "login-vpc" {
   cidr_block       = var.vpc_cidr
-  instance_tenancy = var.tenancy
+  instance_tenancy = var.vpc_tenancy
 
   tags = {
     Name = var.vpc_name
